c685f98378ed30b846043983d2e747bd86575c15
Jeremy Shank <jpshank2@gmail.com>
2019-12-02 15:58:51 -0600

    updated for text input

--- Components/Pages/SearchScreen.js
+++ Components/Pages/SearchScreen.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 export default class SearchScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: "",
+            value: "Search for Recipes with a specific Ingredient"
+        }
+    }
+
     static navigationOptions = {
         title: "Search",
+    }
+
+    handleText = (e) => {
+        this.setState({text: e.target.value})
     }
     
     render() {
         return (
             <View style={searchScreen.container}>
                 <Text>Searchy searchy</Text>
+                <TextInput 
+                    style={searchScreen.input}
+                    value={this.state.value}
+                    onChangeText={this.handleText} />
             </View>
         )
     }
@@ -22,4 +39,9 @@ const searchScreen = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1
+    }
 });
