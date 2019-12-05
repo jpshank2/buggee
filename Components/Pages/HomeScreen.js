b2bdfbd9047cae617afe4943370039b88b4633d4
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2019-12-05 13:39:48 -0600

    First commit

--- Components/Pages/HomeScreen.js
+++ Components/Pages/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native'
 
 export default class HomeScreen extends Component {
     static navigationOptions = {
@@ -9,7 +9,16 @@ export default class HomeScreen extends Component {
     render() {
         return (
             <View style={homeScreen.container}>
-                <Text>Hey there</Text>
+                <ImageBackground source={require('../images/whitePlate.jpg')} style={homeScreen.headerImage}>
+                    <Text style={homeScreen.headerText}>Buggee</Text>
+                    <Text style={homeScreen.headerSubText}>From Recipe to Pantry</Text>
+                </ImageBackground>
+                <View style={homeScreen.iconContainer}>
+                    <Image style={homeScreen.eachIcon} source={require('../images/cookBook.jpeg')}></Image>
+                    <Image style={homeScreen.eachIcon} source={require('../images/magnifyingGlass.jpg')}></Image>
+                    <Image style={homeScreen.eachIcon} source={require('../images/shoppinglist2.png')}></Image>
+                    <Image style={homeScreen.eachIcon} source={require('../images/catProfile.jpg')}></Image>
+                </View>
             </View>
         )
     }
@@ -18,8 +27,54 @@ export default class HomeScreen extends Component {
 const homeScreen = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
+
+    headerImage: {
+        width: '100%',
+        display: 'flex',
+        textAlign: 'center',
+        flex: 1,
+        borderBottomWidth: 3,
+        borderBottomColor: 'red',
+      
+    },
+
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 27,
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        marginTop: '14%',
+        color: 'white',
+        fontFamily: 'ChalkboardSE-Bold' 
+    },
+
+    headerSubText: {
+        fontSize:15,
+        display: 'flex',
+        fontWeight: '100',
+        textAlign: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        fontFamily: 'ChalkboardSE-Bold'
+    },
+
+    iconContainer: {
+        flex: 2,
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
+        alignItems: "center",
+        flexDirection: 'row',
+        alignContent: 'space-around',
+        bottom: 22,
+        // borderBottomColor: 'red',
+        // borderBottomWidth: 3,
+    },
+
+    eachIcon: {
+        width: 150,
+        height: 150,  
+        borderRadius: 30,
+    }
 });
