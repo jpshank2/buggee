1f42347341e0a3886f7a910fe6dd1324517c7eda
Jake Lovett <lovett_jp@outlook.com>
2019-12-09 21:09:34 -0600

    oh god please work

--- client/Components/Pages/BookmarkedScreen.js
+++ client/Components/Pages/BookmarkedScreen.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React, { Component, Fragment } from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { SearchBar, Icon } from 'react-native-elements';
+
+import Heart from '../Fragments/Heart'
 
 export default class BookmarkedScreen extends Component {
     static navigationOptions = {
@@ -9,35 +11,108 @@ export default class BookmarkedScreen extends Component {
 
     render() {
         return (
-            <View style={bookmarkedScreen.container}>
-                <View style={bookmarkedScreen.searchContainer}>
-                    <View style={bookmarkedScreen.searchBar}>
-                        <View style={bookmarkedScreen.input}>
-                            <TextInput placeholder='Search Your Recipes' style={{ fontSize: 15, paddingLeft: 15 }} />
-                            <Icon name='search'></Icon>
+            <Fragment>
+                <View style={bookmarkedScreen.container}>
+                    <View style={bookmarkedScreen.searchContainer}>
+                        <View style={bookmarkedScreen.searchBar}>
+                            <View style={bookmarkedScreen.input}>
+                                <TextInput placeholder='Search Your Recipes' style={{ fontSize: 15, paddingLeft: 15 }} />
+                                <Icon name='search'></Icon>
+                            </View>
+                            <Text style={{ marginTop: 5 }}>These are your favorite recipes</Text>
                         </View>
                     </View>
                 </View>
-                <Text>These are your favorite recipes</Text>
-            </View>
-        )
+
+                <ScrollView>
+                    <View 
+                    style={bookmarkedScreen.recipeBG}
+                    >
+
+                        <View
+                            style={bookmarkedScreen.recipe}
+                        // key={recipe.recipe.uri}
+                        >
+                            <Image
+                                style={bookmarkedScreen.image}
+                                source={{ uri: 'https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg' }}
+                            />
+                            <View
+                                style={{
+                                    flexDirection: "row",
+                                    justifyContent: "center"
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        flexDirection: "column",
+                                        opacity: 1,
+                                        flexWrap: "wrap",
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            textAlign: "center",
+                                            fontSize: 22,
+                                            fontWeight: "500",
+                                            color: "#000",
+                                            paddingLeft: 5,
+                                            flexWrap: "wrap",
+                                        }}
+                                    >
+                                        {`Chicken Vesuvio`}
+                                    </Text>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            this.props.navigation.navigate("Recipe", {
+                                                r: id
+                                            });
+                                        }}
+                                    >
+                                        <Text
+                                            style={{
+                                                textAlign: "center",
+                                                fontSize: 20,
+                                                fontWeight: "300",
+                                                margin: 5,
+                                                paddingLeft: 6
+                                            }}
+                                        >
+                                            Details
+                          </Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
+                                    <Heart />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
+            </Fragment >
+        );
     }
 }
 
 const bookmarkedScreen = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#a0a0a0',
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: 'red',
-        borderBottomWidth: 2,
+        maxHeight: 100,
+        marginBottom: 6,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "gray",
+        shadowRadius: 1,
+        shadowOpacity: 0.35
     },
     searchContainer: {
         flex: 1,
-        alignSelf: 'center'
+        alignSelf: 'center',
+        
     },
-
+    
     searchBar: {
         height: 40,
         margin: 20,
@@ -48,7 +123,7 @@ const bookmarkedScreen = StyleSheet.create({
         borderTopWidth: 2.5,
         borderRadius: 20,
     },
-
+    
     input: {
         borderRadius: 20,
         height: 35,
@@ -56,5 +131,47 @@ const bookmarkedScreen = StyleSheet.create({
         flexDirection: 'row',
         padding: 5,
         alignItems: 'center',
-    }
+    },
+    recipeBG: {
+            flex: 1,
+            backgroundColor: "#fff",
+            alignContent: "center",
+            justifyContent: "center",
+            marginLeft: 14,
+            marginRight: 8,
+            display: "flex",
+            flexWrap: "wrap",
+            minWidth: "85%",
+            maxWidth: "90%",
+    },
+    recipe: {
+        justifyContent: "center",
+        alignContent: "center",
+        backgroundColor: "#ddd",
+        borderRadius: 15,
+        padding: 2,
+        marginBottom: 10,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "gray",
+        shadowRadius: 1,
+        shadowOpacity: 0.2,
+        display: "flex",
+        flexWrap: "wrap",
+        maxWidth: "95%"
+    },
+    image: {
+        width: 200,
+        height: 200,
+        maxWidth: 200,
+        maxHeight: 200,
+        alignSelf: "center",
+        // margin: 5,
+        // padding: 5,
+        borderRadius: 15,
+        opacity: 1,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "gray",
+        shadowRadius: 1,
+        shadowOpacity: 0.35
+    },
 });
